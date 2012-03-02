17c6ca37ac9e9a9e729876a2e72189aae9288014
Ryan Clark <ryan.clark.j@gmail.com>
2012-03-01 19:52:13 -0700

    Began to flesh out the FeatureTypeList property. Everything works but the next step is parsing schema docs to work through DescribeFeatureType requests.

--- WfsClient.cs
+++ WfsClient.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Xml;
 using System.Xml.XPath;
 using System.Xml.Linq;
-using System.Net;
 
 namespace owslib
 {
@@ -20,28 +19,30 @@ namespace owslib
             XElement capabilitiesXml = XElement.Load(capabilitiesXmlReader);
 
             // XmlNamespaceManager has to be populated before it is useful in resolving XPath expressions
-            this.nsManager = new XmlNamespaceManager(capabilitiesXmlReader.NameTable);
-            this.nsManager.AddNamespace("wfs", "http://www.opengis.net/wfs");
-            this.nsManager.AddNamespace("ogc", "http://www.opengis.net/ogc");
-            this.nsManager.AddNamespace("gml", "http://www.opengis.net/gml");
-            this.nsManager.AddNamespace("ows", "http://www.opengis.net/ows");
-            this.nsManager.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            nsManager = new XmlNamespaceManager(capabilitiesXmlReader.NameTable);
+            nsManager.AddNamespace("wfs", "http://www.opengis.net/wfs");
+            nsManager.AddNamespace("ogc", "http://www.opengis.net/ogc");
+            nsManager.AddNamespace("gml", "http://www.opengis.net/gml");
+            nsManager.AddNamespace("ows", "http://www.opengis.net/ows");
+            nsManager.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
 
             // Gather ServiceIdentification Information
-            XElement serviceIdentificationXml = capabilitiesXml.XPathSelectElement("/ows:ServiceIdentification", this.nsManager);
-            this.serviceIdentification = new ServiceIdentification(serviceIdentificationXml, this.nsManager);
+            XElement serviceIdentificationXml = capabilitiesXml.XPathSelectElement("/ows:ServiceIdentification", nsManager);
+            serviceIdentification = new ServiceIdentificationClass(serviceIdentificationXml, nsManager);
 
             // TODO: Gather OperationsMetadata
 
-            // TODO: Gather FeatureTypeList information
+            // Gather FeatureTypeList information
+            IEnumerable<XElement> featureTypes = capabilitiesXml.XPathSelectElements("/wfs:FeatureTypeList/wfs:FeatureType", nsManager);
+            featureTypeList = new FeatureTypeListClass(featureTypes, nsManager);
 
             // TODO: Gather FilterCapabilities
         }
 
         // Classes to house information from the service's GetCapabilities
-        class ServiceIdentification
+        public class ServiceIdentificationClass
         {
-            public ServiceIdentification(XElement serviceIndentificationXml, XmlNamespaceManager nsManager)
+            public ServiceIdentificationClass(XElement serviceIndentificationXml, XmlNamespaceManager nsManager)
             {
                 title = serviceIndentificationXml.XPathSelectElement("ows:Title", nsManager).Value;
                 description = serviceIndentificationXml.XPathSelectElement("ows:Abstract", nsManager).Value;
@@ -71,17 +72,92 @@ namespace owslib
             public string AccessConstraints { get { return accessConstraints; } }
         }
 
-        class ServiceProvider { }
+        public class ServiceProviderClass { }
 
-        class OperationsMetadata { }
+        public class OperationsMetadataClass { }
 
-        class FeatureTypeList { }
+        public class FeatureTypeClass 
+        {
+            public FeatureTypeClass(XElement featureTypeXml, XmlNamespaceManager nsManager)
+            {
+                name = featureTypeXml.XPathSelectElement("wfs:Name", nsManager).Value;
+                title = featureTypeXml.XPathSelectElement("wfs:Title", nsManager).Value;
+                description = featureTypeXml.XPathSelectElement("wfs:Abstract", nsManager).Value;
+                defaultSrs = featureTypeXml.XPathSelectElement("wfs:DefaultSRS", nsManager).Value;
+                foreach (XElement ele in featureTypeXml.XPathSelectElements("wfs:OutputFormats/wfs:Format", nsManager)) { outputFormats.Add(ele.Value); }
+                boundingBox = new BoundingBoxClass(featureTypeXml.XPathSelectElement("ows:WGS84BoundingBox", nsManager), nsManager);
 
-        class FilterCapabilities { }
+                // TODO: Read DescribeFeatureType results for this FeatureType
+            }
+
+            public class BoundingBoxClass
+            {
+                public BoundingBoxClass(XElement boundingBoxXml, XmlNamespaceManager nsManager)
+                {
+                    string lowerCorner = boundingBoxXml.XPathSelectElement("ows:LowerCorner", nsManager).Value;
+                    string upperCorner = boundingBoxXml.XPathSelectElement("ows:UpperCorner", nsManager).Value;
+                    string[] lowerCoords = lowerCorner.Split(new Char[] { ' ' });
+                    string[] upperCoords = upperCorner.Split(new Char[] { ' ' });
+                    northBound = float.Parse(upperCoords[1]);
+                    eastBound = float.Parse(upperCoords[0]);
+                    southBound = float.Parse(lowerCoords[1]);
+                    westBound = float.Parse(lowerCoords[0]);
+                }
+
+                private float northBound;
+                private float southBound;
+                private float eastBound;
+                private float westBound;
+
+                public float NorthBound { get { return northBound; } }                
+                public float SouthBound { get { return southBound; } }                
+                public float EastBound { get { return eastBound; } }                
+                public float WestBound { get { return westBound; } }
+            }
+
+            private string name;
+            private string title;
+            private string description;
+            private string defaultSrs;
+            private List<string> outputFormats = new List<string>();            
+            private BoundingBoxClass boundingBox;
+
+            public string Name { get { return name; } }            
+            public string Title { get { return title; } }            
+            public string Description { get { return description; } }            
+            public string DefaultSrs { get { return defaultSrs; } }
+            public List<string> OutputFormats { get { return outputFormats; } }
+            public BoundingBoxClass BoundingBox { get { return boundingBox; } }
+        }
+
+        public class FeatureTypeListClass : List<FeatureTypeClass>
+        {
+            public FeatureTypeListClass(IEnumerable<XElement> featureTypes, XmlNamespaceManager nsManager)
+            {                
+                foreach (XElement ele in featureTypes) { this.Add(new FeatureTypeClass(ele, nsManager)); }
+            }
+        }
+
+        public class FilterCapabilitiesClass { }
 
         // Fields of the WfsClient
-        XmlNamespaceManager nsManager;
-        private ServiceIdentification serviceIdentification;
+        private XmlNamespaceManager nsManager;
+        private ServiceIdentificationClass serviceIdentification;
+        private OperationsMetadataClass operationsMetadata;
+        private FeatureTypeListClass featureTypeList;
+        private FilterCapabilitiesClass filterCapabilities;
+
+        // Read-only properties of the WfsClient
+        public ServiceIdentificationClass ServiceIdentification { get { return serviceIdentification; } }
+        public OperationsMetadataClass OperationsMetadata { get { return operationsMetadata; } }
+        public FeatureTypeListClass FeatureTypeList { get { return featureTypeList; } }
+        public FilterCapabilitiesClass FilterCapabilities { get { return filterCapabilities; } }
+
+        // Methods
+        public void GetFeature() 
+        {
+            // TODO: Implement this!
+        }
 
     }
 }
